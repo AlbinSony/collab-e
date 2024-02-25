5308a2b39ab73cca4cb5cc065ed360b718471dcd
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2024-02-25 23:06:21 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main>
+    <h1 className="text-4xl">HELLO</h1>
+    <Button>HEYY</Button>
+    </main>
+  );
+}
