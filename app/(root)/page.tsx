95964b65b3fb624857dd150ecb7072ffbf233666
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2024-05-02 14:00:27 +0530

    h img changed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -34,7 +34,7 @@ export default async function Home({searchParams} : SearchParamProps)
           </Button>
         </div>
         <Image 
-        src="/assets/images/hero.png"
+        src="/assets/images/dp1.jpg"
         alt="hero"
         width={1000}
         height={1000}
