d1817a654a56d32442f19d3cd4202e719cb1721b
Albin Sony <96093138+AlbinSony@users.noreply.github.com>
2024-05-02 14:56:02 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -35,7 +35,7 @@ export default async function Home({searchParams} : SearchParamProps)
         </div>
         <Image 
         src="/assets/images/hero.png"
-        alt="hero"
+        alt="hero image"
         width={1000}
         height={1000}
         className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
