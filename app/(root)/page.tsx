68651cea6f417e312022a5fde479eda6e45e3919
Albin Sony <96093138+AlbinSony@users.noreply.github.com>
2024-07-06 01:00:42 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -62,20 +62,17 @@ export default async function Home({searchParams} : SearchParamProps)
           totalPages={events?.totalPages}
         />
     </section>
-    <script>
-      window.embeddedChatbotConfig = {
-      chatbotId: "kFhAklwqg8V9R5cyZKiqM",
-      domain: "www.chatbase.co"
-    };
-    </script>
-
-    <script
-      src="https://www.chatbase.co/embed.min.js"
-      chatbotId="kFhAklwqg8V9R5cyZKiqM"
-      domain="www.chatbase.co"
-      defer>
-    </script>
-
+       <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.embeddedChatbotConfig = {
+              chatbotId: "kFhAklwqg8V9R5cyZKiqM",
+              domain: "www.chatbase.co",
+            };
+          `,
+        }}
+        defer
+      />
     </>
   );
 }
